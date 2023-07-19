013e10ab4fcf893483f2e4648d4fec089693a937
YZ775 <yuzuki-mimura@cybozu.co.jp>
2023-07-19 17:58:55 +0900

    update

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"strconv"
@@ -16,21 +18,40 @@ func main() {
 		panic(err)
 	}
 	summaryAPIEndpoint := fmt.Sprintf("http://127.0.0.1:8001/api/v1/nodes/%s/proxy/stats/summary", nodeName)
+	// kubeSummaryExporterAPIEndpoint := fmt.Sprintf("http://127.0.0.1:9779/node/%s", nodeName)
+
+	// ApiPath := fmt.Sprintf("/api/v1/nodes/%s/proxy/stats/summary", nodeName)
 
 	fmt.Printf("sending %d requests/sec to %s\n...\n", NUM_REQUESTS, summaryAPIEndpoint)
+	// fmt.Printf("executing %d ops/sec to %s\n...\n", NUM_REQUESTS, ApiPath)
+
+	// req.Header.Set("X-Prometheus-Scrape-Timeout-Seconds", "20")
+
 	for {
 		wg := sync.WaitGroup{}
 		for i := 0; i < NUM_REQUESTS; i++ {
 			wg.Add(1)
 			go func() {
-				res, err := http.Get(summaryAPIEndpoint)
+				// err := exec.Command("kubectl", "get", "--raw", ApiPath).Run()
+				// if err != nil {
+				// 	fmt.Println(err)
+				// }
+
+				// res, err := http.Get(kubeSummaryExporterAPIEndpoint)
+
+				ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+				defer cancel()
+				req, _ := http.NewRequestWithContext(ctx, "GET", summaryAPIEndpoint, nil)
+				client := new(http.Client)
+				res, err := client.Do(req)
 				if err != nil {
 					fmt.Println(err)
 					wg.Done()
 					return
 				}
 				if res.StatusCode != 200 {
-					fmt.Printf("statusCode:%s\n", res.Status)
+					b, _ := io.ReadAll(res.Body)
+					fmt.Printf("statusCode:%d, %v\n", res.StatusCode, string(b))
 				}
 				defer res.Body.Close()
 				wg.Done()
